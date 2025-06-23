1ebfa05c274ac93a124222294ae3ae6a6cdb72ef
Ishita Singla <ishita799@gmail.com>
2025-06-24 02:36:45 +0530

    temp changes - testing

--- vector_store_in_memory.py
+++ vector_store_in_memory.py
@@ -50,7 +50,7 @@ vector_store.add_documents(
 
 print("done: doc inserted in qdrant(in memory)")
 
-query = "which part of earth receives maximum solar radiation?"
+query = "how many years before present did the common ancestor of all present life forms arose on earth?"
 results = vector_store.similarity_search(query, k=4)
 
 # qdrant results
